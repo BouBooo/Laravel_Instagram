6608fdce59ac303e21917b4fb6593ace84b394a2
Florent NIOLAS <florent.nicolas@ynov.com>
2019-11-07 23:58:48 +0100

    First route

---
+++ resources/views/profiles/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-4 text-center">
+                <img class="rounded-circle" src="https://cdn.iconscout.com/icon/free/png-512/laravel-226015.png" width="214px"/>
+        </div>
+        <div class="col-8">
+            <div class="d-flex">
+                <div class="h4 mr-3 pt-3">laravel_official</div>
+                <button class="btn btn-sm btn-primary">
+                    S'abonner
+                </button>
+            </div>
+            <div class="d-flex mt-3">
+                <div class="mr-3"><strong>X</strong> publications </div>
+                <div class="mr-3"><strong>X</strong> abonnés</div>
+                <div class="mr-3"><strong>X</strong> abonnements</div>
+            </div>
+            <div class="mt-3">
+                <div>Laravel Framework</div>
+                <div>Love beautiful code ? We do too.</div>
+                <a href="https://laravel.com/">https://laravel.com</a>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-cdg2-1.cdninstagram.com/vp/562837a9984e8d46b52c309f0bcf0db6/5E539DEE/t51.2885-15/e35/c257.0.566.566a/67931335_2418708398406578_3844675548587379486_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=102"/>
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-cdg2-1.cdninstagram.com/vp/ca318a13d9b1ea9b8e61673566b9e6fb/5E4ED318/t51.2885-15/e35/c257.0.566.566a/s480x480/67653691_370427033644330_8590012401461939192_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=110"/>
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-cdg2-1.cdninstagram.com/vp/7ff3ee78e8199b28646baecb7a266aaf/5E4F30B4/t51.2885-15/e35/s480x480/55872688_267250210831063_3238927549301159035_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=107"/>
+        </div>
+    </div>
+</div>
+@endsection
